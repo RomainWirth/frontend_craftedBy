0dadc8383361b2e8689bb87e002605fd63f6105e
Romain WIRTH <romain@89labs.io>
2024-02-21 16:19:47 +0100

    feat: UPD burger menu + dropdown, ADD dependencies & delete unused dependencies"

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,19 +1,57 @@
 <script>
+export default {
+  data() {
+    return {
+      isBurgerIcon: true
+    };
+  },
+  methods: {
+    toggleEvent() {
+      this.isBurgerIcon = !this.isBurgerIcon;
+    }
+  }
+}
 </script>
 
 <template>
   <header class="fixed bottom-0 z-10 w-full flex">
     <div class="w-full flex justify-between p-[2%]">
-      <RouterLink to="/">LOGO</RouterLink>
-      <RouterLink to="/profile">Logo Profil</RouterLink>
-      <RouterLink to="/cart">Logo Panier</RouterLink>
+      <label class="swap swap-rotate">
+        <!-- this hidden checkbox controls the state -->
+        <input type="checkbox" class="theme-controller" value="synthwave" />
+        <!-- sun icon -->
+        <i-ph-sun class="swap-on fill-current w-6 h-6" />
+        <!-- moon icon -->
+        <i-ph-moon class="swap-off fill-current w-6 h-6" />
+      </label>
+      <RouterLink to="/">
+          <img src="../assets/images/logo_base.png" alt="" class="w-[48px] h-[48px]">
+      </RouterLink>
+      <RouterLink to="/profile">
+        <div>
+          <i-ph-user-circle />
+        </div>
+      </RouterLink>
+      <RouterLink to="/cart">
+        <div>
+          <i-ph-shopping-cart />
+        </div>
+      </RouterLink>
       <nav>
-        <ul>
-          <li><RouterLink to="/">Home</RouterLink></li>
-          <li><RouterLink to="/catalog">Catalogue</RouterLink></li>
-          <li><RouterLink to="/artisan">Artisans</RouterLink></li>
-          <li><RouterLink to="/about">About</RouterLink></li>
-        </ul>
+        <details class="dropdown dropdown-top dropdown-end">
+          <summary role="button" class="btn" @click="toggleEvent">
+              <!-- hamburger icon -->
+              <i-ph-list v-if="isBurgerIcon"/>
+              <!-- close icon -->
+              <i-ph-x v-else />
+          </summary>
+          <ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 w-52">
+            <li><RouterLink to="/">Home</RouterLink></li>
+            <li><RouterLink to="/catalog">Catalogue</RouterLink></li>
+            <li><RouterLink to="/artisan">Artisans</RouterLink></li>
+            <li><RouterLink to="/about">About</RouterLink></li>
+          </ul>
+        </details>
       </nav>
     </div>
   </header>
--- src/components/ui/HeadBand.vue
+++ src/components/ui/HeadBand.vue
@@ -1,7 +1,19 @@
-<script></script>
+<script>
+// import {IconLogo} from '@/components/icons/IconLogo.vue';
+
+// export default {
+//   component: {
+//     IconLogo,
+//   },
+// }
+</script>
 
 <template>
-  <div class="headBand">
-    <p>LOGO</p>
+  <div class="relative">
+    <p class="fixed top-0 z-[1]">
+      <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" fill="none"><circle cx="48" cy="48" r="48" fill="#F4F8FA"/>
+    <path stroke="#F2B1A2" stroke-linejoin="round" stroke-width="3" d="M51.67 23.553 45.689 19.2 20.16 66.687l5.185 4.353m26.326-47.487L76.8 71.04H58.452M51.67 23.553 25.345 71.04m33.107 0L51.67 58.772l-2.548 4.353m9.329 7.915-9.329-7.915M25.345 71.04h19.146l4.632-7.915"/>
+  </svg>
+    </p>
   </div>
 </template>
