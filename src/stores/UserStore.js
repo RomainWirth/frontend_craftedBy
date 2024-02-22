54bb091cd03fdb22582967632c83804899552c26
Romain WIRTH <romain@89labs.io>
2024-02-22 17:05:49 +0100

    feat: ADD files to handle users

---
+++ src/stores/UserStore.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('UserStore', {
+  state: () => ({
+    users: [],
+    user: {}
+  })
+})
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,3 +1,7 @@
+<script>
+
+</script>
+
 <template>
   <div class="profile">
     <p>Profil Utilisateur</p>
