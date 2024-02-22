c406f6646cd62f16be3591a8ab9cac861a611894
Romain WIRTH <romain@89labs.io>
2024-02-22 14:30:08 +0100

    UPD footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -3,13 +3,10 @@
 
 
 <template>
-  <footer>
-    FOOTER
+  <footer class="w-full h-[500px] pb-16 bg-dark text-tertiary-light flex justify-center items-center">
+    <p>FOOTER</p>
   </footer>
 </template>
 
 <style>
-footer {
-  background-color: blue;
-}
 </style>
